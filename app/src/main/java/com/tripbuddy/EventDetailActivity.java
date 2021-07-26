6bee0912042be837776315e74f4b543ba61e6eff
jessica lin <53272628+jesslin02@users.noreply.github.com>
2021-07-26 16:21:37 -0700

    added implicit intents for phone and website

--- app/src/main/java/com/tripbuddy/EventDetailActivity.java
+++ app/src/main/java/com/tripbuddy/EventDetailActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
